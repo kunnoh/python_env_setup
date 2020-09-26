c496d8df882952d3f095ff99dcfd3f15c1bfa064
root <root@kunnoh.com>
2020-09-26 16:18:00 +0300

    changed colors

--- install.sh
+++ install.sh
@@ -9,30 +9,30 @@ GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 RESET="\033[0m"
 
-echo -e "${YELLOW}"*********************************
-echo -e "${YELLOW}"[*] "${CYAN}"Checking python version installed currently on the system..."${BOLD_GREEN}"
+echo  "${YELLOW}"*********************************
+echo  "${YELLOW}"[*] "${CYAN}"Checking python version installed currently on the system..."${BOLD_GREEN}"
 python3 --version
-echo -e "${BOLD_GREEN}"[+] "${RESET}""${CYAN}"Creating directory to hold all Python virtual environments"${RESET}"
+echo  "${BOLD_GREEN}"[+] "${RESET}""${CYAN}"Creating directory to hold all Python virtual environments"${RESET}"
 mkdir -p "${HOME}"/virtualenvs
-echo -e "${YELLOW}"[*] "${RESET}""${CYAN}"Downloading pysetenv"${BLUE}${PURPLE}"
+echo "${YELLOW}"[*] "${RESET}""${CYAN}"Downloading pysetenv"${BLUE}${PURPLE}"
 
 curl -# https://raw.githubusercontent.com/kunnoh/python_env_setup/master/py_setup.sh -o ${HOME}/.py_setup.sh
 
 if [ -e "${HOME}/.bashrc" ];
 then
-    echo -e "${BOLD_GREEN}"[+] "${CYAN}"Adding "${GREEN}"~/.bash_profile"${RESET}"
+    echo "${BOLD_GREEN}"[+] "${CYAN}"Adding "${GREEN}"~/.bash_profile"${RESET}"
     echo "source ~/.py_setup.sh" >> ${HOME}/.bashrc
 elif [ -e "${HOME}/.bash_profile" ];
 then
-    echo -e "${BOLD_GREEN}"[+] "${CYAN}"Adding "${GREEN}"~/.bash_profile"${RESET}"
+    echo "${BOLD_GREEN}"[+] "${CYAN}"Adding "${GREEN}"~/.bash_profile"${RESET}"
     echo "source ~/.py_setup.sh" >> ${HOME}/.bash_profile
 fi
 
 # Installation complete
-echo -e "${YELLOW}"[*] "${CYAN}"Installation Completed Successfully!
-echo -e "${YELLOW}"[*] "${CYAN}"Type: "${BLUE}" 'source ~/.bashrc'
-echo -e "${YELLOW}"[*] "${CYAN}"Or open new terminal and start using "${BOLD_GREEN}"pysetenv
+echo "${YELLOW}"[*] "${CYAN}"Installation Completed Successfully!
+echo "${YELLOW}"[*] "${CYAN}"Type: "${BOLD_GREEN}" 'source ~/.bashrc'
+echo "${YELLOW}"[*] "${CYAN}"Or open new terminal and start using "${BOLD_GREEN}"pysetenv
 
 # Usage
 echo -e "${YELLOW}"*********************************
-echo -e "${GREEN}"Usage: pysetenv "${BOLD_GREEN}"VIRTUAL_ENVIRONMENT_NAME"${RESET}"
+echo -e "${GREEN}"Usage: "${BOLD_GREEN}"pysetenv VIRTUAL_ENVIRONMENT_NAME"${RESET}"
